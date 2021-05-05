6da94771f743ab5b9bad870de41180341ea95c76
Ryan81@126.com <wysb19811007>
2021-05-05 21:09:11 +0800

    commit

---
+++ springboot-mail/src/main/java/com/jzweb/springbootmail/service/MailService.java
@@ -0,0 +1,9 @@
+package com.jzweb.springbootmail.service;
+
+public interface MailService {
+    void sendSimpleMail(String to, String subject, String content);
+
+    void sendHtmlMail(String to, String subject, String content);
+
+    void sendAttachmentsMail(String to, String subject, String content, String filePath);
+}
